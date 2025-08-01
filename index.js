5738aa8500da64dcbe597c45e4ee4d383db84182
Lemaire Thomas <lemairetomtom@gmail.com>
2025-08-01 11:52:30 +0200

    ajout test soustraction

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function addition(a, b) {
   return a + b;
 }
+function soustraction(a, b) {
+  return a - b;
+}
 
 module.exports = addition;
+module.exports = soustraction;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,10 @@
 const addition = require('./index');
+const soustraction = require('./index');
 
 test('addition de 2 et 3 doit donner 5', () => {
   expect(addition(2, 3)).toBe(5);
+});
+
+test('soustraction de 5 et 3 doit donner 2', () => {
+  expect(soustraction(5, 3)).toBe(2);
 });
